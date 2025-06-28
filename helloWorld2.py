68b74e663e106c56480896377f25893a2afafe66
Real Name <ailopez9403@gmail.com>
2025-06-28 11:00:42 -0700

    Added median func

--- helloWorld2.py
+++ helloWorld2.py
@@ -1,2 +1,12 @@
 print("Hello, world 2 (edit from VS)")
-print("Hello, world 2 (edited from GH)")
+
+def median(input):
+  length = len(input)
+
+  if length % 2 == 1:
+    median = input[math.floor(length / 2)]
+  else:
+    middle1 = input[math.floor(length / 2)]
+    middle2 = input[(math.floor(length / 2)) - 1]
+    median = (middle1 + middle2) / 2
+  return median
